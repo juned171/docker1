ec269ba3ebab7e43d0a694d1e28a2a41d478441b
juned <junedansari771@gmail.com>
2021-08-27 18:19:31 +0530

    commit

--- Desktop/projects_code/docker_node/deploy.sh
+++ Desktop/projects_code/docker_node/deploy.sh
@@ -3,3 +3,4 @@ git add .
 git commit -m "commit " 
 
 echo " done!"
+aws ec2-instance-connect send-ssh-public-key --instance-id i-0f1d50928d9a6b181 --availability-zone ap-south-1a  --instance-os-user ec2-user --ssh-public-key file://my_rsa_key.pub
